6f642cd2dc2a35d8dee4d097d878b987ef4d8bca
itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-09-30 12:00:00 +0530

    Update UsersPage.jsx

--- frontend/src/pages/UsersPage.jsx
+++ frontend/src/pages/UsersPage.jsx
@@ -6,13 +6,6 @@ import StatCard from "../components/common/StatCard";
 import UsersTable from "../components/users/UsersTable";
 
 
-const userStats = {
-	totalUsers: 152845,
-	newUsersToday: 243,
-	activeUsers: 98520,
-	churnRate: "2.4%",
-};
-
 const UsersPage = () => {
 	return (
 		<div className='flex-1 overflow-auto relative z-10 h-screen'>
