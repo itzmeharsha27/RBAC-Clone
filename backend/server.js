3c2f5e678e051ac03237dde080ceaeacc7ac6a12
itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-09-30 12:00:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieparser from 'cookie-parser';
-import DbCon from './Utils/db.js';
-import AuthRoutes from './Routes/Auth.js';
-import AuthAdmin from './Routes/AdminRoute.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import DbCon from './Utils/db.js'
+import AuthRoutes from './Routes/Auth.js'
+import AuthAdmin from './Routes/AdminRoute.js'
+import cookieparser from 'cookie-parser'
 
-dotenv.config();
+dotenv.config()
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-DbCon();
+DbCon()
 
-app.use(express.json());
+
+app.use(express.json())
 app.use(cors({
-  credentials: true,
-  origin: 'rbac-steel.vercel.app'
-}));
-app.use(cookieparser());
+    credentials: true,
+    origin : "http://localhost:5173"
+}))
+app.use(cookieparser())
 
-app.use('/api/auth', AuthRoutes);
-app.use('/api/admin', AuthAdmin);
+app.use('/api/auth',AuthRoutes)
+app.use('/api/admin',AuthAdmin)
 
-app.get('/', (req, res) => {
-  res.send('test');
-});
 
-// Exporting the express app to Vercel
-export default app;
+app.get('/',(req,res)=>{
+    res.send('test')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
