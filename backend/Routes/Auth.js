83d4241cf560228061b376bd9adeb83c43687ef6
itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-09-30 12:00:00 +0530

    Update Auth.js

--- backend/Routes/Auth.js
+++ backend/Routes/Auth.js
@@ -3,6 +3,8 @@ import { checkuser, getstudents, getTeachers, register } from '../Controllers/Au
 import { login } from '../Controllers/Auth.js'
 import { logout } from '../Controllers/Auth.js'
 import { isuser } from '../MiddleWare/VerifyToken.js'
+import { getTschedule } from '../Controllers/TeacherS.js'
+import { getClassSchedule } from '../Controllers/ClassS.js'
 
 const AuthRoutes = express.Router()
 
@@ -12,5 +14,7 @@ AuthRoutes.post('/logout',logout)
 AuthRoutes.get('/getteachers',getTeachers)
 AuthRoutes.get('/getstudents',getstudents)
 AuthRoutes.get('/checkuser',isuser,checkuser)
+AuthRoutes.get('/teacherS/:tId',getTschedule)
+AuthRoutes.get('/classS/:classId',getClassSchedule)
 
 export default AuthRoutes
