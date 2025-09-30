196912950bdd14234b6f8e77c6de1b1d88cde23e
itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-09-30 12:00:00 +0530

    Updated ApiEndPoint.js

--- frontend/src/services/ApiEndPoint.js
+++ frontend/src/services/ApiEndPoint.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance =axios.create({
-    baseURL:'http://localhost:4000',
+    baseURL:'https://rbac-backend-484l.onrender.com',
     headers:{
         'Content-Type' : 'application/json'
     },
