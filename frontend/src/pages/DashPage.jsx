68526558688089fe142fba65d4ba0dfbeb007a02
itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-09-30 12:00:00 +0530

    Update DashPage.jsx

--- frontend/src/pages/DashPage.jsx
+++ frontend/src/pages/DashPage.jsx
@@ -20,7 +20,7 @@ const DashBoard = () => {
 	useEffect(() => {
 		
 		if (!user) {
-		  navigate('/login');
+		  navigate('/');
 		}
 	  }, [user, navigate]);
 
@@ -64,7 +64,6 @@ const DashBoard = () => {
 			<Header title="Dashboard" />
 
 			<main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
-				{/* STATS */}
 				<motion.div
 					className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
 					initial={{ opacity: 0, y: 20 }}
